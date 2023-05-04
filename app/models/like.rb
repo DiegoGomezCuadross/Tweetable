006747a8f6f1bccf92f677c61d25425b3c308df3
Diego Gomez <diegogc@LAPTOP-8ABLQL06>
2023-05-04 18:25:53 -0500

    add: validates

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
+
+ validates :user, uniqueness: { scope: :tweet, message: "and Tweet combination already taken" }
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,12 @@
 class Tweet < ApplicationRecord
-  # Associations
+  # Validations
+  validates :body, presence:true, allow_blank: false,  length: { maximun: 140 }
+  validates :username, presence: true, uniqueness: true, allow_blank: false
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }, allow_blank: false
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :password, presence: true, length: { minimun: 6 }
   
+  # Associations
   belongs_to :user
   belongs_to :replied_to
   
