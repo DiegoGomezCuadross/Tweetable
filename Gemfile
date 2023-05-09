a9cce1e16c84443d5d70ab1d2921c1ad553ab973
Diego Gomez <diegogc@LAPTOP-8ABLQL06>
2023-05-09 00:30:18 -0500

    add: sign in git hub

--- Gemfile
+++ Gemfile
@@ -56,6 +56,8 @@ gem 'pry'
 
 gem "dotenv-rails"
 
+gem "omniauth-rails_csrf_protection"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
+         
   # Validations
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
@@ -18,4 +19,19 @@ class User < ApplicationRecord
 
   has_many :likes, dependent: :destroy
   has_many :tweets, through: :likes, counter_cache: true
+
+    def self.from_omniauth(auth_hash)
+    # Retorna un usuario si lo encuentra
+    # Si no lo encuentra
+      # Crea al usuario
+      # Luego lo retorna
+
+    where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |user|
+      user.provider = auth_hash.provider
+      user.uid = auth_hash.uid
+      user.username = auth_hash.info.name
+      user.email = auth_hash.info.email
+      user.password = Devise.friendly_token
+    end
+  end
 end
